203ace89661f257ae4ead0ebd76525497d142044
HannaPalianytsia <hannapalianytsia1@gmail.com>
2024-08-14 14:10:17 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation.jsx";
+import Loader from "./components/Loader/Loader.jsx";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage.jsx"));
+const FavoritesPage = lazy(() =>
+  import("./pages/FavoritesPage/FavoritesPage.jsx")
+);
+const NotFoundPage = lazy(() =>
+  import("./pages/NotFoundPage/NotFoundPage.jsx")
+);
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
